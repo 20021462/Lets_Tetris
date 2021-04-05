b8dd88f19afb634e21f2f4c9580bcad1057432b7
LnG <pleasewaitlong@gmail.com>
2021-04-05 12:21:52 +0700

    1

--- Block.cpp
+++ Block.cpp
@@ -11,43 +11,49 @@ Block::Block(Block_type type):
 		matrix[0][1] = Z;
 		matrix[1][1] = Z;
 		matrix[1][2] = Z;
-		
+		this->size = 3;
 		break;
 	case S:
 		matrix[0][1] = L;
 		matrix[0][2] = L;
 		matrix[1][0] = L;
 		matrix[1][1] = L;
+		this->size = 3;
 		break;
 	case J:
 		matrix[0][0] = J;
 		matrix[1][1] = J;
 		matrix[1][2] = J;
 		matrix[1][0] = J;
+		this->size = 3;
 		break;
 	case L:
 		matrix[0][2] = L;
 		matrix[1][1] = L;
 		matrix[1][2] = L;
 		matrix[1][0] = L;
+		this->size = 3;
 		break;
 	case O:
 		matrix[0][0] = O;
 		matrix[0][1] = O;
 		matrix[1][0] = O;
 		matrix[1][1] = O;
+		this->size = 2;
 		break;
 	case I:
-		matrix[0][0] = I;
-		matrix[0][1] = I;
-		matrix[0][2] = I;
-		matrix[0][3] = I;
+		matrix[1][0] = I;
+		matrix[1][1] = I;
+		matrix[1][2] = I;
+		matrix[1][3] = I;
+		this->size = 4;
 		break;
 	case T:
-		matrix[0][0] = T;
-		matrix[0][1] = T;
-		matrix[0][2] = T;
+		matrix[1][0] = T;
 		matrix[1][1] = T;
+		matrix[1][2] = T;
+		matrix[0][1] = T;
+		this->size = 3;
 		break;
 	default:
 		break;
@@ -67,7 +73,11 @@ void Block::move(int x, int y)
 
 void Block::rotate(int matrix[4][4])
 {
-
+	int temp[4][4];
+	if (matrix == NULL) matrix = this->matrix;
+	copyMatrix(matrix, temp, this->size);
+	horizontalReflectioin(matrix,this->size);
+	transpose(matrix, this->size);
 }
 
 int Block::x()
--- Block.h
+++ Block.h
@@ -17,12 +17,13 @@ public:
 	Block(Block_type type);
 	~Block();
 	void move(int x, int y);
-	void rotate(int matrix[4][4]);
+	void rotate(int matrix[4][4]=NULL);
 	int x();
 	int y();
 	
 	
 private:
+	int size;
 	int x_;
 	int y_;
 	int matrix[4][4] = { {0} };
