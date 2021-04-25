e8a7050dba1a6c0c74fa6e3ead26eefc4629d31c
LnG <pleasewaitlong@gmail.com>
2021-04-25 20:01:59 +0700

    main 10 lines

---
+++ game.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "render.h"
+#include "const.h"
+#include "block.h"
+#include "playerField.h"
+#include "button.h"
+#include "sound.h"
+
+class game
+{
+public:
+	game();
+	~game();
+
+	bool quitMode = false;
+	bool quit = false;
+	SDL_Event e;
+
+	void play();
+private:
+
+};
+
+
